7f218a30513f3b68b14c3a1962f89132bdcb4690
unknown <arvindverma9352@gmail.com>
2026-09-19 00:33:50 +0530

    Host VM Delivery Captain independently on Render backend at /delivery/ to eliminate domain scope collision

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/delivery', express.static(path.join(__dirname, 'delivery')));
 app.use(express.static(path.join(__dirname, '..')));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://arvind:arvind5299@cluster0.wnav3z6.mongodb.net/?appName=Cluster0')
